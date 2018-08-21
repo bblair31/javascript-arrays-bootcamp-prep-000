6b1c3ac25dc7b4d25f0d4128bb6e1dfd660ad349
jbb341@hotmail.com <jbb341@hotmail.com>
2018-08-21 21:00:36 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -28,3 +28,7 @@ function destructivelyRemoveElementFromEndOfArray (chocolateBars) {
   chocolateBars.pop()
   return chocolateBars
 }
+function removeElementFromEndOfArray (chocolateBars) {
+  chocolateBars.slice(0,chocolateBars.length - 1)
+  return chocolateBars
+}
