d46c6bcb3677138776a6456030a1114c1ef08beb
jbb341@hotmail.com <jbb341@hotmail.com>
2018-08-21 20:18:24 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,23 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
+
+function addElementToBeginningOfArray(chocolateBars, foo) {
+  return ["foo", ...chocolateBars]
+}
+function destructivelyAddElementToBeginningOfArray(chocolateBars, foo) {
+  chocolateBars.unshift("foo")
+  return chocolateBars
+}
+function addElementToEndOfArray(chocolateBars, foo) {
+  return [...chocolateBars,"foo"]
+}
+function destructivelyAddElementToEndOfArray(chocolateBars, foo) {
+  chocolateBars.push("foo")
+  return chocolateBars
+}
+function accessElementInArray(chocolateBars, Array) {
+  return (chocolateBars,3)
+}
+function destructivelyRemoveElementFromBeginningOfArray() {
+  chocolateBars.shift()
+  return chocolateBars
+}
